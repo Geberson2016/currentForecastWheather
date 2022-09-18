059579920b6ee88dd1e36411b21eb580c81a5b1f
Geberson Antonio <geberson2016@gmail.com>
2022-09-18 12:16:08 -0300

    Commit Domingo

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { City } from './../model/city.model';
 import { Weather } from './../model/weather.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { AirQuality } from '../model/air.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +11,27 @@ import { HttpClient } from '@angular/common/http'
 export class WeatherService {
   foreCast: string = '';
   geocoding: string = '';
+  airQuality: string = '';
 
 
   constructor(private http: HttpClient) {
     this.foreCast = `https://api.open-meteo.com/v1/forecast?daily=temperature_2m_max,temperature_2m_min&timezone=America%2FSao_Paulo`
 
     this.geocoding = `https://geocoding-api.open-meteo.com/v1/search?`
+
+    this.airQuality = `https://air-quality-api.open-meteo.com/v1/air-quality?&hourly=carbon_monoxide,dust`
    }
 
-   getWeather(latitude:string, logitude:string, dateSelected:string, date:string){
-    return this.http.get<Weather>(`${this.foreCast}&latitude=${latitude}&longitude=${logitude}&start_date=${dateSelected}&end_date=${date}`)
+   getWeather(latitude:string, logitude:string, startDate:string, endDate:string){
+    return this.http.get<Weather>(`${this.foreCast}&latitude=${latitude}&longitude=${logitude}&start_date=${startDate}&end_date=${endDate}`)
    }
 
    getLocalization(city:string){
     return this.http.get<City>(`${this.geocoding}&name=${city}`)
 
+   }
+
+   getAirQuality(latitude:string, logitude:string, startDate:string, endDate:string){
+    return this.http.get<AirQuality>(`${this.airQuality}&latitude=${latitude}&longitude=${logitude}&start_date=${startDate}&end_date=${endDate}`)
    }
 }
--- src/app/wheatherHome/home/home.component.ts
+++ src/app/wheatherHome/home/home.component.ts
@@ -1,27 +1,35 @@
+import { AirQuality } from './../../model/air.model';
 import { City } from './../../model/city.model';
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { WeatherService } from '../../services/weather.service'
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 import { Weather } from 'src/app/model/weather.model';
-import { debounceTime, takeUntil } from "rxjs/operators";
+import { debounceTime, takeUntil } from 'rxjs/operators';
 import { fromEvent, pipe, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  @ViewChild('searchCityRef', {static: true}) searchCityRef: ElementRef;
+  @ViewChild('searchCityRef', { static: true }) searchCityRef: ElementRef;
 
   searchCitySub: Subscription;
 
   date: Date;
   weather: Weather;
-  city: City
+  city: City;
+  airQuality: AirQuality;
 
   nameCity = '';
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit() {
     this.nameCity = '';
@@ -29,52 +37,65 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.searchCitySub = fromEvent(this.searchCityRef.nativeElement, 'keyup')
-    .pipe(debounceTime(1500))
-    .subscribe((e:any) => {
-      this.nameCity = e.target.value;
-      this.changeLocalization(this.nameCity);
-    })
+      .pipe(debounceTime(1500))
+      .subscribe((e: any) => {
+        this.nameCity = e.target.value;
+        this.changeLocalization(this.nameCity);
+      });
   }
 
-  changeLocalization(newValue:any) {
-    this.nameCity = newValue
-    this.weatherService.getLocalization(this.nameCity).pipe(debounceTime(1000)).subscribe(
-      res => {
-        this.city = res
-      },
-      err => console.log(err)
-    )
-    console.log(this.nameCity)
-
+  changeLocalization(newValue: any) {
+    this.nameCity = newValue;
+    this.weatherService
+      .getLocalization(this.nameCity)
+      .pipe(debounceTime(1000))
+      .subscribe(
+        (res) => {
+          this.city = res;
+        },
+        (err) => console.log(err)
+      );
   }
 
-  getWeather(){
-    let dateBase = this.date.toISOString().replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
-    let dateResult = dateBase.slice(0,10);
+  getForecastWheather() {
+    let endDateBase = new Date();
+    endDateBase.setDate(this.date.getDate() + 4);
+    let startDate = this.date
+      .toISOString()
+      .replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3')
+      .slice(0, 10);
+    let endDate = endDateBase
+      .toISOString()
+      .replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3')
+      .slice(0, 10);
 
-
-    this.weatherService.getWeather(this.city.results[0].latitude, this.city.results[0].longitude, dateResult, '2022-09-21').subscribe(
-      res => {
-        console.log(res);
-        this.weather = res
-      },
-      err => console.log(err)
-    )
+    this.weatherService
+      .getWeather(
+        this.city.results[0].latitude,
+        this.city.results[0].longitude,
+        startDate,
+        endDate
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.weather = res;
+        },
+        (err) => console.log(err)
+      );
+    this.weatherService
+      .getAirQuality(
+        this.city.results[0].latitude,
+        this.city.results[0].longitude,
+        startDate,
+        endDate
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.airQuality = res;
+        },
+        (err) => console.log(err)
+      );
   }
-
-  // submitLocation(nameCity:HTMLInputElement) {
-
-  //   let dateBase = this.date.toISOString().replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
-  //   let dateResult = dateBase.slice(0,10);
-
-
-
-  //   this.getWeather(this.city.results[0].latitude , this.city.results[0].longitude, dateResult);
-
-  //   nameCity.value = '';
-  //   nameCity.focus();
-
-  //   return false
-  // }
-
 }
